ba1df28a197557d54be6e30ea37dc5df3dd29ab9
Igor Ferreira <igorcferreira@hotmail.com>
2019-11-11 23:43:58 -0300

    Abstract the build of the source publisher

---
+++ Sources/JIFView/Model/DataLoader.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  
+//
+//  Created by Igor Ferreira on 11/11/2019.
+//
+
+#if canImport(Combine)
+import Foundation
+import Combine
+
+struct DataLoader {
+    func buildDataStream(for source: Source) -> AnyPublisher<Data, Never> {
+        switch source {
+        case .local(let data):
+            return self.buildLocalDataStream(for: data)
+        case .remote(let url, let session, let cache):
+            return self.buildRemoteDataStream(for: url, session: session, cache: cache)
+        }
+    }
+    
+    private func buildLocalDataStream(for data: Data) -> AnyPublisher<Data, Never> {
+        return Just(data).eraseToAnyPublisher()
+    }
+    
+    private func buildRemoteDataStream(for url: URL, session: URLSession, cache: ImageCache?) -> AnyPublisher<Data, Never> {
+        
+        let id = url.pathComponents.last ?? url.host ?? UUID().uuidString
+        
+        let cachePublisher = Just(cache?.load(id: id))
+            .tryMap({ try $0.tryUnwrap() })
+        
+        let remotePublisher = session
+            .dataTaskPublisher(for: url)
+            .map({ $0.data })
+            .handleEvents(receiveOutput: {
+                cache?.save(data: $0, withId: id)
+            })
+        
+        let basePublisher = cachePublisher.catch({ _ in remotePublisher })
+            .replaceError(with: Data())
+        
+        return basePublisher.eraseToAnyPublisher()
+    }
+}
+
+#endif
