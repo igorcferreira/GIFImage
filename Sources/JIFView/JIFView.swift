67bbf76ee1bc9cc4d2316882537426c2673195dc
Igor Ferreira <igorcferreira@hotmail.com>
2019-11-11 23:09:28 -0300

    Replace init to use the new data source information

--- Sources/JIFView/JIFView.swift
+++ Sources/JIFView/JIFView.swift
@@ -19,23 +19,17 @@ public struct JIFView: View {
     private let placeholder: UIImage
     private let frameRate: TimeInterval
     private let loop: Bool
-    private let url: URL
-    private let cache: ImageCache?
-    private let session: URLSession
+    private let source: Source
     private let viewQueue = DispatchQueue(label: "SwiftUI.View.JIFView", qos: .background)
     
-    public init(url: URL,
-         placeholder: UIImage? = nil,
-         frameRate: TimeInterval = 0.03,
-         loop: Bool = true,
-         session: URLSession = .shared,
-         cache: ImageCache? = nil) {
+    public init(source: Source,
+                placeholder: UIImage? = nil,
+                frameRate: TimeInterval = 0.03,
+                loop: Bool = true) {
         self.placeholder = placeholder ?? UIImage()
         self.loop = loop
         self.frameRate = frameRate
-        self.url = url
-        self.cache = cache
-        self.session = session
+        self.source = source
     }
     
     private var imageView: some View {
@@ -48,11 +42,15 @@ public struct JIFView: View {
     
     public var body: some View {
         imageView.onAppear(perform: {
-            self.load(url: self.url)
+            self.load()
         })
     }
     
-    private func load(url: URL) {
+    private func load() {
+        
+        guard case .remote(let url, let session, let cache) = source else {
+            return
+        }
         
         guard subscriber == nil, let id = url.pathComponents.last ?? url.host else {
             return
@@ -65,7 +63,7 @@ public struct JIFView: View {
             .dataTaskPublisher(for: url)
             .map({ $0.data })
             .handleEvents(receiveOutput: {
-                self.cache?.save(data: $0, withId: id)
+                cache?.save(data: $0, withId: id)
             })
         
         let basePublisher = cachePublisher.catch({ _ in remotePublisher })
@@ -85,8 +83,15 @@ public struct JIFView: View {
 }
 
 struct JIFView_Preview: PreviewProvider {
+    
+    static var source: Source {
+        return .remote(url: URL(string: "https://images.squarespace-cdn.com/content/v1/50ff1acce4b047a6c7999c73/1566186157381-T7WSLE7DPT8DG1VMWDBK/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVH-2yKxPTYak0SCdSGNKw8A2bnS_B4YtvNSBisDMT-TGt1lH3P2bFZvTItROhWrBJ0/Llama_Run_Instagram_.gif")!,
+                       session: .shared,
+                       cache: nil)
+    }
+    
     static var previews: some View {
-        JIFView(url: URL(string: "https://images.squarespace-cdn.com/content/v1/50ff1acce4b047a6c7999c73/1566186157381-T7WSLE7DPT8DG1VMWDBK/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVH-2yKxPTYak0SCdSGNKw8A2bnS_B4YtvNSBisDMT-TGt1lH3P2bFZvTItROhWrBJ0/Llama_Run_Instagram_.gif")!)
+        JIFView(source: source)
     }
 }
 #endif
