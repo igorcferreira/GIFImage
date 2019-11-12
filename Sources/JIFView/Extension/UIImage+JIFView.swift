48264bb7640d856a38a55ed5cb0cd5cd029867da
Igor Ferreira <igorcferreira@hotmail.com>
2019-11-12 00:10:26 -0300

    Create support files for macOS

---
+++ Sources/JIFView/Extension/UIImage+JIFView.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Igor Ferreira on 12/11/2019.
+//
+
+import Foundation
+#if canImport(UIKit)
+import UIKit
+#else
+import AppKit
+#endif
+
+extension UIImage {
+    static func build(with cgImage: CGImage) -> UIImage {
+        #if canImport(UIKit)
+        return UIImage(cgImage: cgImage)
+        #else
+        let size = NSSize(width: cgImage.width, height: cgImage.height)
+        return UIImage(cgImage: cgImage, size: size)
+        #endif
+    }
+}
